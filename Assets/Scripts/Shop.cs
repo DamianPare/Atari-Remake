d5cc9e6af490b73f5b3c27576d93444e4c8eacd8
DamianPare <damianpare@shaw.ca>
2024-10-20 00:39:59 -0700

    Updating scripts

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] int upgradeCost;
+    private int money;
+
+    private void Update()
+    {
+        money = GameManager.instance.money;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (money > upgradeCost)
+        {
+            GameManager.instance.RemoveMoney(upgradeCost);
+            GameManager.instance.Upgrade();
+            upgradeCost *= 2;
+            Debug.Log(upgradeCost);
+        }
+        
+        else
+        {
+            Debug.Log("Not Enough Money");
+        }
+    }
+}
